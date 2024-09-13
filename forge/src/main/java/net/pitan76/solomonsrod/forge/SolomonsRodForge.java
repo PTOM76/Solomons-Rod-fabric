9a7a61c03a411d09c4feeac130945aaf6d9e2941
PTOM76 <58260965+PTOM76@users.noreply.github.com>
2024-09-13 16:06:50 +0900

    onSetupClient

--- forge/src/main/java/net/pitan76/solomonsrod/forge/SolomonsRodForge.java
+++ forge/src/main/java/net/pitan76/solomonsrod/forge/SolomonsRodForge.java
@@ -1,15 +1,25 @@
 package net.pitan76.solomonsrod.forge;
 
 import dev.architectury.platform.forge.EventBuses;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.pitan76.solomonsrod.SolomonsRod;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.pitan76.solomonsrod.SolomonsRodClient;
 
 @Mod(SolomonsRod.MOD_ID)
 public class SolomonsRodForge {
     public SolomonsRodForge() {
-        // Submit our event bus to let architectury register our content on the right time
-        EventBuses.registerModEventBus(SolomonsRod.MOD_ID, FMLJavaModLoadingContext.get().getModEventBus());
+        IEventBus bus = FMLJavaModLoadingContext.get().getModEventBus();
+        EventBuses.registerModEventBus(SolomonsRod.MOD_ID, bus);
+
+        bus.addListener(SolomonsRodForge::onSetupClient);
+
         new SolomonsRod();
+    }
+
+    public static void onSetupClient(FMLClientSetupEvent event) {
+        SolomonsRodClient.init();
     }
 }
